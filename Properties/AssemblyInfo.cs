ab63f5256ed3d66893f9c7a2fb1e074c06306242
CRWolff <27215580+crwolff@users.noreply.github.com>
2022-07-25 11:33:28 -0600

    Cleanup build warning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Security;
 using System.Security.Permissions;
 
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+//[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
